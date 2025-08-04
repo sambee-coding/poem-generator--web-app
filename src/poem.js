de702ea65ccc64f7fdff4e5a4f4602471ca9b415
sambee coding <samrisamrawit30@@email.com>
2025-08-03 18:19:14 -0700

    auto-commit

--- src/poem.js
+++ src/poem.js
@@ -3,6 +3,9 @@ function peomGeneration(event){
     let topicElement=document.querySelector(".inputValue").value;
     document.querySelector(".topic").textContent=topicElement;
   
+let poemStart=document.querySelector(".poem");
+poemStart.style.display="block";
+poemStart.innerHTML=`<div class="blink">🌱 Generating your poem with ${topicElement} title </div>`;
 
       let apiKey = "2faae9d4e47d0b0a09a9to05afdf381d";
   let prompt = `Write a beautiful and creative poem about "${topicElement}"  in english. Maximum 8 lines and separate each line with <br>.`;
@@ -11,15 +14,20 @@ function peomGeneration(event){
   axios.get(apiUrl).then(function (response) {
     let poemText = response.data.answer;
 
+
+      let blinkElement = document.querySelector(".blink");
+        if (blinkElement) {
+            blinkElement.style.display = "none";
+        }
    let poemTarget = document.querySelector("#the-poem");
     poemTarget.textContent = ""; // clear old poem
 
     new Typewriter(poemTarget, {
       strings: [poemText],
       autoStart: true,
-      delay: 1,
+      delay: 50,
       cursor: "",
-      pauseFor:2000,
+      pauseFor:4000,
     });
   });
 }
