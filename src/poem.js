9c3c83f2d752b2199cd223111e2b7d9308ce1a3c
sambee coding <samrisamrawit30@@email.com>
2025-08-06 13:11:12 -0700

    i added somthing

--- src/poem.js
+++ src/poem.js
@@ -4,23 +4,23 @@ function peomGeneration(event){
     document.querySelector(".topic").textContent=topicElement;
   
 let poemStart=document.querySelector(".poem");
-poemStart.style.display="block";
+poemStart.classList.remove("hidden");
 poemStart.innerHTML=`<div class="blink">🌱 Generating your poem with ${topicElement} title </div>`;
+
+
 
       let apiKey = "2faae9d4e47d0b0a09a9to05afdf381d";
   let prompt = `Write a beautiful and creative poem about "${topicElement}"  in english. Maximum 8 lines and separate each line with <br>.`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&key=${apiKey}`;
-
-  axios.get(apiUrl).then(function (response) {
+    axios.get(apiUrl).then(function (response) {
     let poemText = response.data.answer;
 
-
+/*
       let blinkElement = document.querySelector(".blink");
         if (blinkElement) {
             blinkElement.style.display = "none";
         }
-   let poemTarget = document.querySelector("#the-poem");
-    poemTarget.textContent = ""; // clear old poem
+  */
 
     new Typewriter(poemTarget, {
       strings: [poemText],
@@ -32,6 +32,8 @@ poemStart.innerHTML=`<div class="blink">🌱 Generating your poem with ${topicEl
   });
 }
 
+ 
+ 
 let poemElement=document.querySelector(".form");
 poemElement.addEventListener("submit",peomGeneration);
 
